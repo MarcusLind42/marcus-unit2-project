b0a0c561889b4374a55580721eb5c7adc083105d
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-05 14:29:00 -0500

    Writing JS to pull data from PokeAPI

--- public/pokemonjs.js
+++ public/pokemonjs.js
@@ -4,30 +4,29 @@ const pokemon_number = 150
 
 
 const fetchPokemon = async () => {
-  for(let i=0; i <= pokemon_number; i++) {
+  for(let i = 1; i <= pokemon_number; i++) {
     await getPokemon(i)
   }
 }
 
 const getPokemon = async id => {
-  const url = 
-  `https://pokeapi.co/api/v2/pokemon/${id}`
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
   const res = await fetch(url)
   const pokemon = await res.json()
   createPokemonCard(pokemon)
 }
 
-fetchPokemon();
 
 function createPokemonCard(pokemon) {
   const pokemonEl = document.createElement('div')
   pokemonEl.classList.add('pokemon')
-
+  
   const pokeInnerHtml = `
-    ${pokemon.id}
+  ${pokemon.id}
   `;
-
+  
   pokemonEl.innerHTML = pokeInnerHtml;
-
+  
   poke_container.appendChild(pokemonEl)
 }
+fetchPokemon();
