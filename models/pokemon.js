4aee7698ba268e4a5bc192a5cf41cd92c57d11a3
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-06 17:21:20 -0500

    Creating DB of all pokemon with info needed

--- models/pokemon.js
+++ models/pokemon.js
@@ -4,6 +4,13 @@ const Schema = mongoose.Schema
 
 const pokemonSchema = new Schema ({
   name: String,
+  height: Number,
+  weight: Number,
+  // need front_default
+  sprite: String,
+  type1: [String],
+  type2: [String],
+  description: String,
 })
 
 const Pokemon = mongoose.model("Pokemon", pokemonSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,50 @@
 import { Router } from 'express'
 import axios from 'axios'
+import fetch from "node-fetch"
+import { Pokemon } from '../models/pokemon.js'
 
 const router = Router()
 
-// router.get('/', function (req, res) {
-//   res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
-// })
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
+})
+
+router.get('/add', async function (req, res) {
+  const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${req.query.pokemon}`)
+
+  const data = await response.json()
+  
+  const pokemon = data
+  
+  const pokemonName = pokemon.name
+  const pokemonHeight = pokemon.height
+  const pokemonWeight = pokemon.weight 
+  const pokemonSprite = pokemon.sprites.front_default
+  const pokemonType1 = pokemon.types[0].type.name
+  const pokemonType2 = pokemon.types[1].type.name
+
+
+  const responseDescription = await fetch(`http://pokeapi.co/api/v2/pokemon-species/${req.query.pokemon}`)
+
+  const data2 = await response.json()
+
+  const pokemonText = data2
+
+  const pokemonDescription = pokemonText.flavor_text_entries[0]
+
+  console.log(responseDescription);
+  
+  Pokemon.create({
+    name: pokemonName,
+    height: pokemonHeight,
+    weight: pokemonWeight,
+    sprite: pokemonSprite,
+    type1: pokemonType1,
+    type2: pokemonType2,
+  })
+  
+  res.redirect('/')
+})
 
 // router.get('/', (req, res) => {
 //   let pokemonUrl = ('https://pokeapi.co/api/v2/pokemon?limit=151')
@@ -15,14 +54,14 @@ const router = Router()
 //     res.render('index', { title: 'Home Page', user: req.user ? req.user : null, pokemonUrl }))
 // })
 
-router.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
-  // Use request to call the API
-  axios.get(pokemonUrl).then(response => {
-    let pokemon = response.data.results;
-    res.render('index', { title: 'Home Page', pokemon: pokemon.slice(0, 151) });
-  });
-});
+// router.get('/', function(req, res) {
+//   let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+//   // Use request to call the API
+//   axios.get(pokemonUrl).then(response => {
+//     let pokemon = response.data.results;
+//     res.render('index', { title: 'Home Page', pokemon: pokemon.slice(0, 151) });
+//   });
+// });
 
 export {
   router
