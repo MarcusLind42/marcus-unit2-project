b96338b6f5a6f6cc74a2ed03396fe73685858868
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-08 13:33:10 -0500

    Adding submit to team button and path

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,19 @@
 import mongoose from 'mongoose'
 
+const Schema = mongoose.Schema
+
+const teamSchema = new Schema ({
+  name: {type: [String], required: true, unique: true},
+}, {
+  timestamps: true
+})
+
+
+
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  pokemonTeam: [{type: Schema.Types.ObjectId, ref: 'Team'}]
 }, {
   timestamps: true
 })
