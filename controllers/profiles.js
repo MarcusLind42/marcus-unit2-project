0006fc13d67e7d1e18da1598b09fed999fbcbee6
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-08 14:29:29 -0500

    Troubleshooting AddToTeam function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -32,9 +32,27 @@ function show(req, res) {
   })
 }
 
-
+function addToTeam(req,res) {
+  Profile.findById(req.params.id)
+  .then((profile) => {
+    Profile.findById(req.user.profile._id)
+    .then(self => {
+      const isSelf = self._id.equals(profile._id)
+      res.render("/pokemon", {
+        profile,
+        self,
+        isSelf
+      })
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+    res.redirect('/pokemon');
+  })
+}
 
 export {
   index,
   show,
+  addToTeam
 }
