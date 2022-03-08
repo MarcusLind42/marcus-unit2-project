3de5e47f7fcd816ebd71bda61e0ed95f0691cb57
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-08 13:44:04 -0500

    Editing pokemon controller for profile functionality

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,10 +1,12 @@
 import { Pokemon } from "../models/pokemon.js"
+import { Profile } from "../models/profile.js"
 import fetch from 'node-fetch' 
 
 function index(req, res) {
   Pokemon.find({})
     .then(pokemon => {
       res.render('pokemon/index', {
+        profiles,
         pokemon,
         title: 'All Pokemon'
       })
@@ -19,6 +21,7 @@ function show(req, res) {
   Pokemon.findById(req.params.id)
   .then(pokemon => {
     res.render('pokemon/show', {
+      profiles,
       pokemon,
       title: "pokemon"
     })
