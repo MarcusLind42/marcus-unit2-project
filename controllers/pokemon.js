b0e4aab38b4abaaebb02114f95b06e46a252ed25
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-09 09:59:50 -0500

    Editing addToTeam controller function to find pokemonteam of profile and update with the pokemon clicked

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -33,7 +33,7 @@ function show(req, res) {
 function addToTeam(req, res) {
   Pokemon.findById(req.params.id)
     .then(pokemon => {
-      PokemonTeam.create({name: req.params.id, trainer: req.user.profile._id})
+      PokemonTeam.findByIdAndUpdate({name: req.params.id})
         .then(pokemonTeam => {
           pokemonTeam.names.push(pokemon);
           pokemonTeam.save()
