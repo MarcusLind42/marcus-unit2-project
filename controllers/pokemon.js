e5049503ee83caa326ca0e978a96a7234f87883e
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-09 10:42:47 -0500

    Testing new addToTeam logic

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -33,7 +33,7 @@ function show(req, res) {
 function addToTeam(req, res) {
   Pokemon.findById(req.params.id)
     .then(pokemon => {
-      PokemonTeam.create({name: req.params.id, trainer: req.user.profile._id})
+      PokemonTeam.find({trainer: req.user.profile._id})
         .then(pokemonTeam => {
           pokemonTeam.names.push(pokemon);
           pokemonTeam.save()
