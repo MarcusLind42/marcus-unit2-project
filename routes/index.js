b210598a4c08de31b0c4b51132638bfb1e306333
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-03-11 10:07:26 -0500

    Editing README.md

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 import { Router } from 'express'
-import axios from 'axios'
 import fetch from "node-fetch"
 import { Pokemon } from '../models/pokemon.js'
 
@@ -11,45 +10,41 @@ router.get('/', function (req, res) {
 
 // What I used to pull data from the PokeAPI to my database
 
-// router.get('/add', async function (req, res) {
-//   const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${req.query.pokemon}`)
+router.get('/add', async function (req, res) {
+  const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${req.query.pokemon}`)
 
-//   const data = await response.json()
+  const data = await response.json()
   
-//   const pokemon = data
+  const pokemon = data
   
-//   const pokemonId = pokemon.id
-//   const pokemonName = pokemon.name
-//   const pokemonHeight = pokemon.height
-//   const pokemonWeight = pokemon.weight 
-//   const pokemonSprite = pokemon.sprites.front_default
-//   const pokemonSpriteShiny = pokemon.sprites.front_shiny
-//   const pokemonType1 = pokemon.types[0].type.name
+  const pokemonId = pokemon.id
+  const pokemonName = pokemon.name
+  const pokemonHeight = pokemon.height
+  const pokemonWeight = pokemon.weight 
+  const pokemonSprite = pokemon.sprites.front_default
+  const pokemonSpriteShiny = pokemon.sprites.front_shiny
+  const pokemonType1 = pokemon.types[0].type.name
 
-//   console.log(pokemonId);
+  const responseDescription = await fetch(`http://pokeapi.co/api/v2/pokemon-species/${req.query.pokemon}`)
 
+  const data2 = await responseDescription.json()
 
-//   const responseDescription = await fetch(`http://pokeapi.co/api/v2/pokemon-species/${req.query.pokemon}`)
+  const pokemonText = data2
 
-//   const data2 = await responseDescription.json()
-
-//   const pokemonText = data2
-
-//   const pokemonDescription = pokemonText.flavor_text_entries[2].flavor_text
+  const pokemonDescription = pokemonText.flavor_text_entries[2].flavor_text
   
-//   Pokemon.create({
-//     pokedexId: pokemonId,
-//     name: pokemonName,
-//     height: pokemonHeight,
-//     weight: pokemonWeight,
-//     sprite: pokemonSprite,
-//     sprite_shiny: pokemonSpriteShiny,
-//     type1: pokemonType1,
-//     description: pokemonDescription
-//   })
-  
-//   res.redirect('/')
-// })
+  Pokemon.create({
+    pokedexId: pokemonId,
+    name: pokemonName,
+    height: pokemonHeight,
+    weight: pokemonWeight,
+    sprite: pokemonSprite,
+    sprite_shiny: pokemonSpriteShiny,
+    type1: pokemonType1,
+    description: pokemonDescription
+  })
+  res.redirect('/')
+})
 
 export {
   router
