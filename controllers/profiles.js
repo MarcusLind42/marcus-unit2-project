f35ce1180508a8693d327d598191ba720e855b0c
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-04-29 11:22:19 -0400

    Editing showSelf function and change UI req trigger href

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,81 +1,81 @@
-import { Profile } from '../models/profile.js'
-import { Pokemon } from '../models/pokemon.js'
-import { PokemonTeam } from '../models/pokemon-team.js'
+import { Profile } from "../models/profile.js";
+import { Pokemon } from "../models/pokemon.js";
+import { PokemonTeam } from "../models/pokemon-team.js";
 
 function index(req, res, next) {
   Profile.find({})
-  .then(profiles => {
-    res.render('profiles/index', {
-      profiles: profiles,
-      title: "All Pokemon Trainers"
+    .then((profiles) => {
+      res.render("profiles/index", {
+        profiles: profiles,
+        title: "All Pokemon Trainers",
+      });
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect(`/`)
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect(`/`);
+    });
 }
 
 function show(req, res) {
   Profile.findById(req.params.id)
-  .populate('team')
-  .then((profile) => {
-    Profile.findById(req.user.profile._id)
-    .then(self => {
-      const isSelf = self._id.equals(profile._id)
-      res.render("profiles/show", {
+    .populate("team")
+    .then((profile) => {
+      Profile.findById(req.user.profile._id).then((self) => {
+        const isSelf = self._id.equals(profile._id);
+        res.render("profiles/show", {
+          title: `${profile.name}'s profile`,
+          profile,
+          self,
+          isSelf,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
+}
+
+function showSelf(req, res) {
+  Profile.findById(req.params.id)
+    .populate('team')
+    .then(profile => {
+      res.render('profiles/show', {
         title: `${profile.name}'s profile`,
         profile,
         self,
         isSelf,
       })
     })
-  })
-  .catch((err) => {
-    console.log(err)
-    res.redirect("/")
-  })
-}
-
-function showSelf(req, res) {
-  Profile.findById(req.params.id)
-  .populate('team')
-
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/pokemon");
+    });
 }
 
 function createTeam(req, res) {
-  PokemonTeam.create({trainer: req.user.profile._id})
-    .then(pokemonteam => {
-      pokemonteam.save()
-        .then(function() {
-          res.redirect('/pokemon')
-        })
-      })
+  PokemonTeam.create({ trainer: req.user.profile._id })
+    .then((pokemonteam) => {
+      pokemonteam.save().then(function () {
+        res.redirect("/pokemon");
+      });
+    })
     .catch((err) => {
-      console.log(err)
-      res.redirect("/")
-  })
+      console.log(err);
+      res.redirect("/");
+    });
 }
 function deleteFromTeam(req, res) {
-  Profile.findById(req.user.profile._id)
-    .then(profile => {
-        Pokemon.findById(req.params.id)
-          .then(pokemon => {
-            const idx = profile.team.indexOf(pokemon._id)
-            console.log('profile.team', profile.team);
-            profile.team.splice(idx, 1)
-            profile.save()
-              .then(() => {
-                res.redirect(`/profiles/${profile._id}`)
-          })
-        })
-    })
+  Profile.findById(req.user.profile._id).then((profile) => {
+    Pokemon.findById(req.params.id).then((pokemon) => {
+      const idx = profile.team.indexOf(pokemon._id);
+      console.log("profile.team", profile.team);
+      profile.team.splice(idx, 1);
+      profile.save().then(() => {
+        res.redirect(`/profiles/${profile._id}`);
+      });
+    });
+  });
 }
 
-export {
-  index,
-  show,
-  createTeam,
-  deleteFromTeam as delete,
-  showSelf,
-}
+export { index, show, createTeam, deleteFromTeam as delete, showSelf };
